9d3c760b273f7ab604d1440a571182cb1770aaef
Chetana Krishnan <63806095+chetana348@users.noreply.github.com>
2025-11-02 11:58:07 -0500

    Clean up metadata in coco_detection.py
    Removed erroneous metadata comments from the file.

--- Stage1/Config/coco_detection.py
+++ Stage1/Config/coco_detection.py
@@ -1,11 +1,3 @@
-'''
-Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
-Date: 2024-01-22 15:08:43
-LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
-LastEditTime: 2024-01-31 08:22:41
-FilePath: /pancreas/MyTGSeg/LSAM/polyp_grounding_dino/config/basic/coco_detection.py
-Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
-'''
 # dataset settings
 dataset_type = 'CocoDataset'
 data_root = 'data/coco/'
